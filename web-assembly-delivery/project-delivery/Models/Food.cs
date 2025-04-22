80a4b335de85eef11949695597d404ad830ad63c
arthurd3 <arthur_camposl@yahoo.com>
2025-04-22 12:19:45 -0300

    feat: make entire food card clickable, remove button, add hover effect and discounted price styling

--- web-assembly-delivery/project-delivery/Models/Food.cs
+++ web-assembly-delivery/project-delivery/Models/Food.cs
@@ -2,6 +2,7 @@ namespace project_delivery.Models
 {
     public class Food
     {
+        public double descountedPrice {get; set;} = 0;
         public string Name { get; set; } = string.Empty;
         TypeFood? typeFood { get; set; } = null; 
         public string Description { get; set; } = string.Empty;
--- web-assembly-delivery/project-delivery/obj/Debug/net9.0/project-delivery.AssemblyInfo.cs
+++ web-assembly-delivery/project-delivery/obj/Debug/net9.0/project-delivery.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project-delivery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cac9d1f9dfa2eeacca0e7dfd99abe254cad50f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d27fa475b33229400138084c54724d52a3f4feb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("project-delivery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project-delivery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
