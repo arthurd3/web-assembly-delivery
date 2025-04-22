eb046ac15d77c71dd8ad20664169a0febac40dd1
arthurd3 <arthur_camposl@yahoo.com>
2025-04-22 01:33:49 -0300

    refactor: decouple and reorganize home interface components

---
+++ web-assembly-delivery/project-delivery/models/Food.cs
@@ -0,0 +1,7 @@
+public class Food
+{
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public double Price { get; set; } = 0;
+}
