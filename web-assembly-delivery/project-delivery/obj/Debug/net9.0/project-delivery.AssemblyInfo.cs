9114914a64814b68675b6d8ee312a671fdfb7966
arthurd3 <arthur_camposl@yahoo.com>
2025-04-21 22:50:01 -0300

    feat: Changes in the code and improvements in the structure

--- web-assembly-delivery/project-delivery/obj/Debug/net9.0/project-delivery.AssemblyInfo.cs
+++ web-assembly-delivery/project-delivery/obj/Debug/net9.0/project-delivery.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project-delivery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79e557a7973c0e3ed783a09b977a2f6ed474a929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2767c3e16d4f7cf588f655283c72ef7ac8a86c6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("project-delivery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project-delivery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
