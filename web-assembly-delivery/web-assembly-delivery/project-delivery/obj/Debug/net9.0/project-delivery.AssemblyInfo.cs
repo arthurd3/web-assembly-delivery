975d9cf67641fcc636b54671e0899c6678e9e538
arthurd3 <arthur_camposl@yahoo.com>
2025-04-24 12:40:19 -0300

    remove images

--- web-assembly-delivery/web-assembly-delivery/project-delivery/obj/Debug/net9.0/project-delivery.AssemblyInfo.cs
+++ web-assembly-delivery/web-assembly-delivery/project-delivery/obj/Debug/net9.0/project-delivery.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project-delivery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b37128d611803fe60163978747bacf7d4a112b57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3fe383ecde398c51b32c9e86f156f2cead991d")]
 [assembly: System.Reflection.AssemblyProductAttribute("project-delivery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project-delivery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
