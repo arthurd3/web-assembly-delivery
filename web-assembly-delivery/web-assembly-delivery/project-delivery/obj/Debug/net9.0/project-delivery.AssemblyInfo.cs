3909b8fec1cd9db57d490f2896f4d7cee76ba91a
arthurd3 <arthur_camposl@yahoo.com>
2025-04-24 14:06:06 -0300

    adjust gitgnore

--- web-assembly-delivery/web-assembly-delivery/project-delivery/obj/Debug/net9.0/project-delivery.AssemblyInfo.cs
+++ web-assembly-delivery/web-assembly-delivery/project-delivery/obj/Debug/net9.0/project-delivery.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project-delivery")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3fe383ecde398c51b32c9e86f156f2cead991d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+975d9cf67641fcc636b54671e0899c6678e9e538")]
 [assembly: System.Reflection.AssemblyProductAttribute("project-delivery")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project-delivery")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
