3dcbc7c4fdf335610c44d63a607b013dc6038cb4
arthurd3 <arthur_camposl@yahoo.com>
2025-06-27 00:14:51 -0300

    feat: create user login

--- web-assembly-delivery/project-delivery-beckend/api-delivery/Program.cs
+++ web-assembly-delivery/project-delivery-beckend/api-delivery/Program.cs
@@ -30,4 +30,22 @@ app.MapGet("/weatherforecast", () =>
     };
 });
 
+app.MapPost("/login", (LoginRequest request) => {
+    
+    if (request.Username == "admin" && request.Password == "123")
+    {
+        
+        return Results.Ok(new { Token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" });
+    }
+    else
+    {
+        
+        return Results.Unauthorized();
+    }
+});
+
+
 app.Run();
+
+
+public record LoginRequest(string Username, string Password);
